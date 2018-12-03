8eeb4ef2d4eb3a3f5df49487fb5add7226fcebd1
Rastko Veris <rastko07@gmail.com>
2018-12-03 11:29:38 +0100

    Povezava na bazo trgovina.sdb

---
+++ modeli.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect('trgovina.sdb')
+conn.execute('PRAGMA foreign_keys = ON')
+
+def commit(fun):
+    """
+    Dekorator, ki ustvari kurzor, ga poda dekorirani funkciji,
+    in nato zapiše spremembe v bazo.
+    Originalna funkcija je na voljo pod atributom nocommit.
+    """
+    def funkcija(*largs, **kwargs):
+        cur = conn.cursor()
+        ret = fun(cur, *largs, **kwargs)
+        conn.commit()
+        cur.close()
+        return ret
+    funkcija.__doc__ = fun.__doc__
+    funkcija.__name__ = fun.__name__
+    funkcija.__qualname__ = fun.__qualname__
+    fun.__qualname__ += '.nocommit'
+    funkcija.nocommit = fun
+    return funkcija
