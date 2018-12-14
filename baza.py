e1e5c212406f5016075e44d0fee6180343ed65e4
Rastko Veris <rastko07@gmail.com>
2018-12-14 22:25:25 +0100

    kreirane tabele

--- baza.py
+++ baza.py
@@ -1,20 +1,22 @@
 import csv
-from modeli import commit
 
-@commit
-def pobrisi_tabele(cur):
+def pobrisi_tabele(conn):
     """
     Pobriše tabele iz baze.
     """
-    cur.execute("DROP TABLE IF EXISTS kategorija;")
-    cur.execute("DROP TABLE IF EXISTS kupec;")
+    conn.execute("DROP TABLE IF EXISTS kategorija;")
+    conn.execute("DROP TABLE IF EXISTS kupec;")
+    conn.execute("DROP TABLE IF EXISTS status;")
+    conn.execute("DROP TABLE IF EXISTS narocilo;")
+    conn.execute("DROP TABLE IF EXISTS izdelek;")
+    conn.execute("DROP TABLE IF EXISTS narocilo_vsebuje_izdelek;")
 
-@commit
-def ustvari_tabele(cur):
+def ustvari_tabele(conn):
     """
     Ustvari tabele v bazi.
+
     """
-    cur.execute("""
+    conn.execute("""
         CREATE TABLE kupec (
     id_kupec        INTEGER PRIMARY KEY AUTOINCREMENT,
     davcna_stevilka VARCHAR NOT NULL
@@ -22,11 +24,13 @@ def ustvari_tabele(cur):
     naslov          VARCHAR NOT NULL,
     ime             VARCHAR NOT NULL
                             UNIQUE,
+    prezime             VARCHAR NOT NULL
+                            UNIQUE,
     kontaktna_oseba VARCHAR
 
         );
     """)
-    cur.execute("""
+    conn.execute("""
         CREATE TABLE kategorija (
     id_kategorija INTEGER PRIMARY KEY AUTOINCREMENT
                           NOT NULL
@@ -34,4 +38,67 @@ def ustvari_tabele(cur):
     ime           VARCHAR NOT NULL
         );
     """)
+
+    conn.execute("""
+        CREATE TABLE status (
+    id_status INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE,
+    naziv_statusa           VARCHAR NOT NULL
+        );
+    """)
+
+    conn.execute("""
+        CREATE TABLE narocilo (
+    id_narocilo INTEGER PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE,
+    datum           DATETIME NOT NULL,
+    rok_placila           DATETIME NOT NULL,
+    kupec_id_kupec REFERENCES kupec(id_kupec),
+    status_id_status REFERENCES status(id_status)
+        );
+    """)
+
+    conn.execute("""
+        CREATE TABLE izdelek (
+            id_izdelek INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+            opis VARCHAR,
+            zaloga INTEGER NOT NULL,
+            cena DOUBLE NOT NULL,
+            kategorija_id_kategorija REFERENCES kategorija(id_kategorija)
+        )
+    """)
+
+    conn.execute("""
+        CREATE TABLE narocilo_vsebuje_izdelek (
+            id_narocilo_vsebuje_izdelek INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+            kolicina INTEGER NOT NULL,
+            popust DOUBLE,
+            izdelek_id_izdelek REFERENCES izdelek(id_izdelek),
+            narocilo_id_narocilo REFERENCES narocilo(id_narocilo)
+        )
+    """)
+
     
+
+def ustvari_bazo(conn):
+    """
+    Opravi celoten postopek postavitve baze.
+    """
+    pobrisi_tabele(conn)
+    ustvari_tabele(conn)
+    # uvozi_filme(conn)
+    # uvozi_osebe(conn)
+    # uvozi_vloge(conn)
+    # uvozi_zanre(conn)
+
+
+def ustvari_bazo_ce_ne_obstaja(conn):
+    """
+    Ustvari bazo, če ta še ne obstaja.
+    """
+    with conn:
+        cur = conn.execute("SELECT COUNT(*) FROM sqlite_master")
+        if cur.fetchone() == (0, ):
+            ustvari_bazo(conn)
--- modeli.py
+++ modeli.py
@@ -1,23 +1,25 @@
+import baza
 import sqlite3
 
-conn = sqlite3.connect('trgovina.sdb')
+conn = sqlite3.connect('trgovina2.sdb')
+baza.ustvari_bazo_ce_ne_obstaja(conn)
 conn.execute('PRAGMA foreign_keys = ON')
 
-def commit(fun):
-    """
-    Dekorator, ki ustvari kurzor, ga poda dekorirani funkciji,
-    in nato zapiše spremembe v bazo.
-    Originalna funkcija je na voljo pod atributom nocommit.
-    """
-    def funkcija(*largs, **kwargs):
-        cur = conn.cursor()
-        ret = fun(cur, *largs, **kwargs)
-        conn.commit()
-        cur.close()
-        return ret
-    funkcija.__doc__ = fun.__doc__
-    funkcija.__name__ = fun.__name__
-    funkcija.__qualname__ = fun.__qualname__
-    fun.__qualname__ += '.nocommit'
-    funkcija.nocommit = fun
-    return funkcija
+# def commit(fun):
+#     """
+#     Dekorator, ki ustvari kurzor, ga poda dekorirani funkciji,
+#     in nato zapiše spremembe v bazo.
+#     Originalna funkcija je na voljo pod atributom nocommit.
+#     """
+#     def funkcija(*largs, **kwargs):
+#         cur = conn.cursor()
+#         ret = fun(cur, *largs, **kwargs)
+#         conn.commit()
+#         cur.close()
+#         return ret
+#     funkcija.__doc__ = fun.__doc__
+#     funkcija.__name__ = fun.__name__
+#     funkcija.__qualname__ = fun.__qualname__
+#     fun.__qualname__ += '.nocommit'
+#     funkcija.nocommit = fun
+#     return funkcija
